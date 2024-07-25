90208482a216994483b92ef96dd1844c72831fdb
Timymur <hitechmag2013@mail.ru>
2024-07-25 09:08:08 +0500

    ignore

--- Calculator/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Calculator.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ed570081f67633f7973278bcbf6ff70971a64422")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
